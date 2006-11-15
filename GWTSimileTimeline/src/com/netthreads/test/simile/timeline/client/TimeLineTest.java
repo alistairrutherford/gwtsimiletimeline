50029ad1dc4099e35c3dd5128a515d9c4148263a
alistair.rutherford <alistair.rutherford@0e916270-5921-0410-8c44-bfc22265d602>
2006-11-15 14:11:28 +0000

    .Fixed timeline creation startup. We were required to set the widget size after it had been attached to the DOM and before it was created for the creation to work. .App resizes correctly under IE.

--- GWTSimileTimeline/src/com/netthreads/test/simile/timeline/client/TimeLineTest.java
+++ GWTSimileTimeline/src/com/netthreads/test/simile/timeline/client/TimeLineTest.java
@@ -4,6 +4,7 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.WindowResizeListener;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.netthreads.gwt.simile.timeline.client.ClientSizeHelper;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -32,15 +33,9 @@ public class TimeLineTest implements EntryPoint, WindowResizeListener
 
         // Load dataset 
         DatasetHandler.getInstance().initialise(this, getInitialDataset());
-        
-        // Call the window resized handler to get the initial sizes setup.
-        // Note there is an issue with the timeline drawing itself into
-        // a div which hasn't been attached to the DOM. The resize will 
-        // ultimately trigger a 'layout' call which seems to generate the
-        // view correctly as long as it's done through _this_ mechanism and
-        // not in the constructor, say, of the timline tab. TODO need to 
-        // get to the bottom of this.
-        onWindowResized(Window.getClientWidth(), Window.getClientHeight());
+
+        // Firefox needs an initial resize 'bump'
+        onWindowResized(ClientSizeHelper.getClientWidth(), ClientSizeHelper.getClientHeight());
     }
 
     /**
@@ -67,11 +62,13 @@ public class TimeLineTest implements EntryPoint, WindowResizeListener
      */
     public void onWindowResized(int width, int height)
     {
-        mainPanel.onWindowResized(width, height);
+        mainPanel.onWindowResized(ClientSizeHelper.getClientWidth(), ClientSizeHelper.getClientHeight());
     }
 
     public static MainTabPanel getMainPanel()
     {
         return mainPanel;
     }
+
+    
 }
